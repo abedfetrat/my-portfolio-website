0273eaae2fc21e398148e3e528be9fd1a8723e25
Abed Fetrat <abedfetrat@gmail.com>
2023-02-24 14:14:35 +0100

    add navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,35 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
   content: ["./*.html"],
   theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px'
+    },
     fontFamily: {
       primary: 'Space Grotesk, sans-serif'
     },
-    extend: {},
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        md: '1.5rem'
+      }
+    },
+    extend: {
+      colors: {
+        primary: colors.emerald[400]
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addVariant }) {
+      addVariant('hocus', ['&:hover', '&:focus'])
+    })
+  ],
 }
