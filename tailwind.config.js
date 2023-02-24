3b54f2c0216b5a01a1e64c26689753a67b38693d
Abed Fetrat <abedfetrat@gmail.com>
2023-02-24 13:08:57 +0100

    setup project

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    fontFamily: {
+      primary: 'Space Grotesk, sans-serif'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
