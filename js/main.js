a17717e33de85048b7d7f9ffd6315ce0cd04fd30
Abed Fetrat <abedfetrat@gmail.com>
2023-03-10 10:16:01 +0100

    improve navbar

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@
 const navBar = document.querySelector('#navbar');
 window.onscroll = () => {
     if (window.scrollY > 50) {
-        navBar.classList.add("bg-white/10");
+        navBar.classList.add("bg-black/20");
         navBar.classList.add("backdrop-blur-md");
     } else {
-        navBar.classList.remove("bg-white/10");
+        navBar.classList.remove("bg-black/20");
         navBar.classList.remove("backdrop-blur-md");
     }
 };
